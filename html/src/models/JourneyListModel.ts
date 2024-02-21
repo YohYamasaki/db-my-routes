df4c7f9fa3916162db432ae0faabf3c1b3ec0ee0
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2024-02-21 21:58:40 +0100

    add departure/arrival toggle

--- html/src/models/JourneyListModel.ts
+++ html/src/models/JourneyListModel.ts
@@ -1,10 +1,12 @@
 import m from "mithril";
 import {journey} from "../types/journey";
+import {parseDatetime} from "../utils/parseDatetime";
 
 interface ITripListModel {
     journeys: journey[],
     fetchJourneys: () => void,
-    toggleNational: (useNational: boolean) => void
+    toggleNational: (useNational: boolean) => void,
+    toggleType: (isArrival: boolean) => void
 }
 
 export class JourneyListModel implements ITripListModel {
@@ -12,12 +14,13 @@ export class JourneyListModel implements ITripListModel {
     private _departureStationId: string;
     private _arrivalStationId: string;
     private _useNational: boolean;
+    private _isArrival: boolean;
 
     constructor(departureStationId: string, arrivalStationId: string, includeNational: boolean) {
         this._departureStationId = departureStationId;
         this._arrivalStationId = arrivalStationId;
         this._useNational = includeNational;
-
+        this._isArrival = false;
     }
 
     get journeys() {
@@ -30,9 +33,16 @@ export class JourneyListModel implements ITripListModel {
             to: this._arrivalStationId,
             nationalExpress: this._useNational ? "true" : "false",
             national: this._useNational ? "true" : "false",
-            results: "5"
-        }).toString();
-        let url = `https://v5.db.transport.rest/journeys?${params}`;
+            results: "5",
+        });
+        // set arrival or departure
+        if (this._isArrival) {
+            params.set("arrival", parseDatetime(Date.now()));
+        } else {
+            params.set("departure", parseDatetime(Date.now()));
+        }
+
+        let url = `https://v5.db.transport.rest/journeys?${params.toString()}`;
         fetch(url).then(res => {
             if (!res.ok) throw Error(`${res.status} ${res.statusText}`);
             return res.json();
@@ -47,5 +57,9 @@ export class JourneyListModel implements ITripListModel {
 
     toggleNational(useNational: boolean) {
         this._useNational = useNational;
+    }
+
+    toggleType(isArrival: boolean) {
+        this._isArrival = isArrival;
     }
 }
--- html/src/views/pages/JourneyListPage.ts
+++ html/src/views/pages/JourneyListPage.ts
@@ -20,7 +20,7 @@ export class JourneyListPage implements ClassComponent<JourneyListPageAttrs> {
     }
 
     view({attrs}: Vnode<JourneyListPageAttrs>): void | Children {
-        return m("div.w-full",
+        return m("div.w-full.min-h-screen",
             [
                 m(Header, {title: "Journey List", showBackButton: true}),
                 m("h2.text-2xl.font-bold.mt-2", `${attrs.departureName} -> ${attrs.arrivalName}`),
@@ -30,20 +30,26 @@ export class JourneyListPage implements ClassComponent<JourneyListPageAttrs> {
                     onChange: (useNational: boolean) => this.model?.toggleNational(useNational),
                     label: "Include ICE/IC"
                 }),
-                // Journey list
-                m("div.py-4", this.model?.journeys.map(journey => m(JourneyTile, {
-                    arrivalStationName: attrs.arrivalName,
-                    departureStationName: attrs.departureName,
-                    journey: journey
-                }))),
+                // Toggle departure or arrival
+                m(ToggleSwitch, {
+                    onChange: (isArrival: boolean) => this.model?.toggleType(isArrival),
+                    label: "Arrival (After current time)"
+                }),
                 // Update data
-                m("button.block.bg-red-800.px-4.py-3.m-auto.border.rounded-sm.font-bold.w-1/2.hover:opacity-60",
+                m("button.block.bg-red-800.px-4.py-2.m-auto.border.rounded-sm.font-bold.w-1/3.hover:opacity-60",
                     {
                         onclick: () => {
                             this.model?.fetchJourneys();
                         },
                     },
                     "Update Data"),
+                // Journey list
+                m("div.py-4", this.model?.journeys.map(journey => m(JourneyTile, {
+                    arrivalStationName: attrs.arrivalName,
+                    departureStationName: attrs.departureName,
+                    journey: journey
+                }))),
+
             ]
         );
     }
