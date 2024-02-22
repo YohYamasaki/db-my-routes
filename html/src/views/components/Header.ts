59408fcf50a44119fa8df60696ff3f40b002cc21
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2024-02-22 21:30:37 +0100

    add header icon

--- html/src/views/components/Header.ts
+++ html/src/views/components/Header.ts
@@ -1,5 +1,7 @@
 import m, {Vnode, ClassComponent} from "mithril";
 import {BackButton} from "./parts/BackButton";
+// @ts-ignore
+import mainIcon from "../../images/icon-192x192.png";
 
 interface HeaderAttrs {
     title: string,
@@ -13,6 +15,7 @@ export class Header implements ClassComponent<HeaderAttrs> {
     view({attrs}: Vnode<HeaderAttrs>) {
         return m("div.relative.flex.justify-center.items-center.w-full.h-11.bg-red-800", [
             attrs.showBackButton && m(BackButton),
+            m("img.w-8.h-8.pr-2", {src: mainIcon}),
             m("h1.text-xl.font-bold", attrs.title),
             attrs.showButton &&
             m("button.ml-auto", {onclick: attrs.onButtonClick}, attrs.buttonText)
