34a13046dc1810d5e94a70cc5b49bff48c24473b
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2024-02-28 13:58:38 +0100

    add AddRouteModel test

--- html/src/models/AddRouteModel.ts
+++ html/src/models/AddRouteModel.ts
@@ -1,50 +1,49 @@
-import {station} from "../types/station";
-import {route} from "../types/route";
+import { station } from "../types/station";
+import { route } from "../types/route";
 
 interface IAddRouteModel {
-    departure: station | undefined,
-    arrival: station | undefined,
-    addRoute: () => void
+  departure: station | undefined;
+  arrival: station | undefined;
+  addRoute: () => void;
 }
 
 export class AddRouteModel implements IAddRouteModel {
-    private _departure: station | undefined;
-    private _arrival: station | undefined;
+  private _departure: station | undefined;
+  private _arrival: station | undefined;
 
-    get departure() {
-        return this._departure;
+  get departure() {
+    return this._departure;
+  }
 
+  get arrival() {
+    return this._arrival;
+  }
+
+  set departure(departure: station | undefined) {
+    this._departure = departure ? { ...departure } : undefined;
+  }
+
+  set arrival(arrival: station | undefined) {
+    this._arrival = arrival ? { ...arrival } : undefined;
+  }
+
+  addRoute() {
+    if (!this._departure || !this._arrival) return;
+
+    const route: route = {
+      departure: this._departure,
+      arrival: this._arrival
+    };
+    let routes: route[] = [];
+    try {
+      // if already routes exists, add new route
+      const routesJson = localStorage.getItem("routes");
+      if (routesJson) routes = JSON.parse(routesJson) as route[];
+      routes.push(route);
+      // set updated routes array to local storage
+      localStorage.setItem("routes", JSON.stringify(routes));
+    } catch (e) {
+      console.error(e);
     }
-
-    get arrival() {
-        return this._arrival;
-    }
-
-    set departure(departure: station | undefined) {
-        this._departure = departure ? {...departure} : undefined;
-    }
-
-    set arrival(arrival: station | undefined) {
-        this._arrival = arrival ? {...arrival} : undefined;
-    }
-
-    addRoute() {
-        if (!this._departure || !this._arrival) return;
-
-        const route: route = {
-            departure: this._departure,
-            arrival: this._arrival
-        };
-        let routes: route[] = [];
-        try {
-            // if already routes exists, add new route
-            const routesJson = localStorage.getItem("routes");
-            if (routesJson) routes = JSON.parse(routesJson) as route[];
-            routes.push(route);
-            // set updated routes array to local storage
-            localStorage.setItem("routes", JSON.stringify(routes));
-        } catch (e) {
-            console.error(e);
-        }
-    }
+  }
 }
---
+++ html/test/AddRouteModel.test.ts
@@ -0,0 +1,75 @@
+import { AddRouteModel } from "../src/models/AddRouteModel";
+import { station } from "../src/types/station";
+
+class LocalStorageMock {
+  private store: Record<string, string>;
+
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+const sampleStation1: station = {
+  id: "8000128",
+  name: "Göttingen"
+};
+
+const sampleStation2: station = {
+  id: "8000152",
+  name: "Hannover Hbf"
+};
+
+describe("AddRouteModel.addRoute()", () => {
+  let model: AddRouteModel;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  beforeEach(() => {
+    // @ts-ignore
+    global.localStorage = new LocalStorageMock();
+  });
+
+  test("should add new route to the localstorage when with the valid departure/arrival", async () => {
+    // Initiate model with Göttingen to Hannover
+    model = new AddRouteModel();
+    model.departure = sampleStation1;
+    model.arrival = sampleStation2;
+
+    // call the target method
+    model.addRoute();
+
+    // assertion
+    expect(localStorage.getItem("routes")).toBe(
+      '[{"departure":{"id":"8000128","name":"Göttingen"},"arrival":{"id":"8000152","name":"Hannover Hbf"}}]'
+    );
+  });
+
+  test("should do nothing when with no departure/arrival data", async () => {
+    // Initiate model with Göttingen to Hannover
+    model = new AddRouteModel();
+
+    // call the target method
+    model.addRoute();
+
+    // assertion
+    expect(localStorage.getItem("routes")).toBe(null);
+  });
+});
