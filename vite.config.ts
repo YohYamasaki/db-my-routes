07165c3f3f3875bb7326beffcc94d2c206e5464b
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2024-08-04 11:33:22 +0200

    fix for vercel hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import {defineConfig} from "vite";
 
 export default defineConfig({
-    base: "/db-my-routes/"
-    // Uncomment to use JSX:
-    // esbuild: {
-    //   jsx: "transform",
-    //   jsxFactory: "m",
-    //   jsxFragment: "'['",
-    // },
+  base: "/",
+  // Uncomment to use JSX:
+  // esbuild: {
+  //   jsx: "transform",
+  //   jsxFactory: "m",
+  //   jsxFragment: "'['",
+  // },
 });
