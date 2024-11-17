2e9627cf097e29bb8c74424281630df7558e5892
YohYamasaki <y.yamasaki@tutanota.com>
2024-11-17 22:09:17 +0100

    change update button position

--- src/views/pages/JourneyListPage.ts
+++ src/views/pages/JourneyListPage.ts
@@ -1,10 +1,10 @@
-import m, {Children, ClassComponent, Vnode} from "mithril";
-import {JourneyListModel} from "../../models/JourneyListModel";
-import {appActions, appState} from "../../states/appState";
-import {Header} from "../components/Header";
-import {JourneyTile} from "../components/JourneyTile";
-import {LoadingOverlay} from "../components/LoadingOverlay";
-import {ToggleSwitch} from "../components/parts/ToggleSwitch";
+import m, { Children, ClassComponent, Vnode } from "mithril";
+import { JourneyListModel } from "../../models/JourneyListModel";
+import { appActions, appState } from "../../states/appState";
+import { Header } from "../components/Header";
+import { JourneyTile } from "../components/JourneyTile";
+import { LoadingOverlay } from "../components/LoadingOverlay";
+import { ToggleSwitch } from "../components/parts/ToggleSwitch";
 
 interface JourneyListPageAttrs {
   departureId: string;
@@ -16,31 +16,41 @@ interface JourneyListPageAttrs {
 export class JourneyListPage implements ClassComponent<JourneyListPageAttrs> {
   private model: JourneyListModel | undefined;
 
-  async oninit({attrs}: Vnode<JourneyListPageAttrs>) {
+  async oninit({ attrs }: Vnode<JourneyListPageAttrs>) {
     // init model
-    this.model = new JourneyListModel(
-      attrs.departureId,
-      attrs.arrivalId,
-      false
-    );
+    this.model = new JourneyListModel(attrs.departureId, attrs.arrivalId, true);
     // fetch journey data
     await this.model.fetchJourneys();
     m.redraw();
   }
 
-  view({attrs}: Vnode<JourneyListPageAttrs>): void | Children {
+  view({ attrs }: Vnode<JourneyListPageAttrs>): void | Children {
     return m("div.w-full.min-h-screen", [
-      m(Header, {title: "Journey List", showBackButton: true}),
-      m(LoadingOverlay, {isShow: appState.isLoading}),
+      m(Header, {
+        title: "Journey List",
+        showBackButton: true,
+        showButton: true,
+        buttonText: "Update",
+        onButtonClick: async () => {
+          // Update data
+          appActions.showLoading();
+          await this.model?.fetchJourneys();
+          appActions.hideLoading();
+          m.redraw();
+        },
+      }),
+      m(LoadingOverlay, { isShow: appState.isLoading }),
       m(
         "h2.text-2xl.font-bold.mt-2",
         `${attrs.departureName} -> ${attrs.arrivalName}`
       ),
       // Toggle National trains
       m(ToggleSwitch, {
-        onChange: (useNational: boolean) =>
-          this.model?.toggleNational(useNational),
+        onChange: (useNational: boolean) => {
+          this.model?.toggleNational(useNational);
+        },
         label: "Include ICE/IC",
+        default: true,
       }),
       // Toggle departure or arrival
       m(ToggleSwitch, {
@@ -49,7 +59,7 @@ export class JourneyListPage implements ClassComponent<JourneyListPageAttrs> {
       }),
       // time picker
       m("div.my-2", [
-        m("label.mr-2", {for: "time-picker"}, "Select Time"),
+        m("label.mr-2", { for: "time-picker" }, "Select Time"),
         m(
           "input#time-picker.text-gray-600.font-bold.px-2.py-1.rounded-md.bg-gray-200",
           {
@@ -61,19 +71,6 @@ export class JourneyListPage implements ClassComponent<JourneyListPageAttrs> {
           }
         ),
       ]),
-      // Update data
-      m(
-        "button.block.bg-red-800.px-4.py-2.m-auto.border.rounded-sm.font-bold.w-1/3.hover:opacity-60",
-        {
-          onclick: async () => {
-            appActions.showLoading();
-            await this.model?.fetchJourneys();
-            appActions.hideLoading();
-            m.redraw();
-          },
-        },
-        "Update Data"
-      ),
       // Journey list
       m(
         "div.py-4",
